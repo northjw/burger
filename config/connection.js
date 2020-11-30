9aadf3e8ece78af435c60f45f224e4cf0a0aefdf
mini pony <northjw1@gmail.com>
2020-11-30 14:31:44 -0500

    creating css layouts

--- config/connection.js
+++ config/connection.js
@@ -2,7 +2,7 @@ var mysql = require("mysql");
 
 var connection = mysql.createConnection({
   host: "localhost",
-  port: 3300,
+  port: 3306,
   user: "root",
   password: "MyMarge1983@",
   database: "burgers_db"
