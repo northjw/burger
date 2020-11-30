723eac135adc063eff25caaae7bb2c1a115272cc
mini pony <northjw1@gmail.com>
2020-11-30 13:21:39 -0500

    inserting burgers into seeds

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('Mushroom Swiss Burger', FALSE);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Chipotle Bacon Burger', FALSE);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Black and Blue Burger', FALSE);
