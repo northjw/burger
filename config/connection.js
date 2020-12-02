cca7915bae0378428927fcbedf296d6d170df33c
mini pony <northjw1@gmail.com>
2020-12-01 19:54:30 -0500

    adding jawsdb info

--- config/connection.js
+++ config/connection.js
@@ -1,11 +1,11 @@
 var mysql = require("mysql");
 
 var connection = mysql.createConnection({
-  host: "localhost",
-  port: 80,
-  user: "root",
-  password: "MyMarge1983@",
-  database: "burgers_db"
+  host: "de1tmi3t63foh7fa.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
+  port: 3306,
+  user: "zuyqkw03mebc88f7",
+  password: "gxb4p0d2kgl44irw",
+  database: "qf99oaguts0n1azj"
 });
 
 
